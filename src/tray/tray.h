59c3d771b55f21bab7d0d8932d2d22c943d18c2d
Pinetree <pine@woodlarch.com>
2026-08-26 17:22:04 -0400

    Revert "Removed c_tray and added it back as a module"
    This reverts commit a25f2841044712e9ed8cb2a40fd96db157269b7b.

---
+++ src/tray/tray.h
@@ -0,0 +1,45 @@
+#ifndef TRAY_H
+#define TRAY_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+struct tray_menu;
+
+struct tray {
+  const char *icon;
+  char *tooltip;
+  struct tray_menu *menu;
+  void *icon_handle;
+  int icon_is_shared;
+  void (*left_click_cb)(void *context);
+  void *left_click_context;
+};
+
+struct tray_menu {
+  const char *text;
+  int disabled;
+  int checked;
+  int checkbox;
+
+  void (*cb)(struct tray_menu *);
+  void *context;
+
+  struct tray_menu *submenu;
+};
+
+int tray_init(struct tray *tray);
+
+int tray_loop(int blocking);
+
+void tray_update(struct tray *tray);
+
+void tray_exit(void);
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
+#endif /* TRAY_H */
